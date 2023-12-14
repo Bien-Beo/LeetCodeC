b0fa0be08ff28bf9ca4629edfecb2bc4a4edffbf
bienbeo <tranngocbien2809@gmail.com>
2023-12-14 16:53:36 +0700

    Edit file and up new file

--- IntersectionTwoArray.cpp
+++ IntersectionTwoArray.cpp
@@ -1,66 +1,57 @@
-//349. Intersection of Two Arrays
-//Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 #include <stdio.h>
-
-void importArr(int arr[], int &length)
-{
-	for(int i = 0; i < length; i++)
-	{
-		scanf("%d", &arr[i]);
-	}
+//Import Array
+void importArr (int arr[], int length) {
+    for (int i = 0; i < length; i++) {
+        scanf("%d", &arr[i]);
+    }
 }
-
-void Intersection(int arr1[], int arr2[], int length1, int length2) {
-	if(length1 >= length2) {
-		int resultArr[length1];
-		int resultSize = 0;
-		for(int i = 0; i < length1; i++) {
-			for(int j = 0; j < length2; j++) {
-				if(arr1[i] == arr2[j]) {
-					resultArr[resultSize] = arr1[i];
-					resultSize += 1;
-				}
-			}
-		}	
-	}
-	else {
-		int resultArr[length2];
-		int resultSize = 0;
-		for(int i = 0; i < length2; i++) {
-			for(int j = 0; j < length1; j++) {
-				if(arr2[i] == arr1[j]) {
-					resultArr[resultSize] = arr2[i];
-					resultSize += 1;
-				}
-			}
-		}
-	}
-	for(int k = 0; k < resultSize; k++) {
-		int temp = resultArr[k];
-		for(int z = 0; z < resultSize; z++) {
-			if(z == k)
-			{
-				z += 1;
-				continue;
-			}
-			else
-			{
-				if(temp == resultArr[z]) {
-					resultArr[k] = resultArr[k + 1];
-					resultSize -= 1;
-				}
-			}
-		}
-	}
-	for(int y = 0; y < resultSize; y++) {
-		printf("%d\t", resultArr[y]);
-	}
+//Funtion Intersection
+void arr_Intersection (int Arr_Intersection[], int arr1[], int length1, int arr2[], int length2) {
+    int count = 0;
+    //Compare elements of two arrays
+    for (int i = 0; i < length1; i++) {
+        for (int j = 0; j < length2; j++) {
+            if (arr1[i] == arr2[j]) { 
+                count++;
+                //Store common elements between two arrays into a new array
+                Arr_Intersection[count-1] = arr1[i]; 
+            }
+        }
+    }
+    
+    if (count == 0) //Case 1: 
+        printf("Array intersection has no element !");
+    else if (count == 1) //Case 2: There is 1 common element
+        printf("%d ", Arr_Intersection[0]);
+    else { //Case 3: There are common elements
+        printf("%d ", Arr_Intersection[0]); //Always output the first matching element
+        for (int i = 1; i < count; i++) {
+            int temp = 0;
+            for (int j = 0; j < i; j++) { 
+            //Check if there are duplicate elements in the new array
+                if (Arr_Intersection[i] == Arr_Intersection[j]) { 
+                    temp++;
+                    continue;
+                }
+                else
+                    continue;   
+            }
+            if (temp == 0) 
+                printf("%d ", Arr_Intersection[i]);
+        }
+    }
 }
+int main() {
+    int size1;
+    printf("\nEnter the size of the 1st array: "); scanf("%d", &size1);
+    int nums1[size1];
+    printf("Enter the number of elements in 1st array: "); importArr(nums1, size1);
 
-int main(){
-	int numSize1; scanf("%d", &numSize1);
-	int nums1[numSize1]; importArr(nums1, numSize1);
-	int numSize2; scanf("%d", &numSize2);
-	int nums2[numSize2]; importArr(nums2, numSize2);
-	Intersection(nums1, nums2, numSize1, numSize2);
+	int size2;
+	printf("\nEnter the size of the 2st array: "); scanf("%d", &size2);
+    int nums2[size2];
+    printf("Enter the number of elements in 2st array: "); importArr(nums2, size2);
+    
+    int Arr_Intersection[size1 + size2];
+    arr_Intersection(Arr_Intersection, nums1, size1, nums2, size2);
 }
---
+++ PalindromeNumber_9.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void splitArr(int x) {
+	if(x )
+}
+
+int main() {
+	int x; 
+	printf("\nEnter number: "); scanf("%d", &x);
+	
+}
