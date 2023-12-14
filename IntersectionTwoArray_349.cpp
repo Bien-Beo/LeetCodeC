197f7b07b5b42deef3e530ce92790fd16c58b26b
bienbeo <tranngocbien2809@gmail.com>
2023-12-14 20:54:35 +0700

    edit file

--- IntersectionTwoArray_349.cpp
+++ IntersectionTwoArray_349.cpp
@@ -1,3 +1,5 @@
+//349. Intersection of Two Array
+//Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 #include <stdio.h>
 //Import Array
 void importArr (int arr[], int length) {
